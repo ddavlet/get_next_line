9cd0d5f2a9e190f3f37dcd9848b7da837ff3299b
Dilshod Davletyarov <ddavlety@c0c2c1.42berlin.de>
2023-11-29 17:53:55 +0100

    tests

--- main_test.c
+++ main_test.c
@@ -1,4 +1,4 @@
-#include "get_next_line_myrepo/get_next_line.h"
+#include "get_next_line.h"
 #include <stdio.h>
 #include <fcntl.h>
 
@@ -6,35 +6,90 @@ int main()
 {
 	char *line;
 	int n;
-	int fd = open("read_error.txt", O_RDONLY);
-	// int fd2 = open("read_error.txt", O_RDONLY);
+	int fd = open("tests/read_error.txt", O_RDONLY);
+	int fd2 = open("tests/1char.txt", O_RDONLY);
+	int fd3 = open("tests/42_no_nl.txt", O_RDONLY);
+	int fd4 = open("tests/one_line_no_nl.txt", O_RDONLY);
+	int fd5 = open("tests/nl.txt", O_RDONLY);
+	int fd6 = open("tests/multiple_nlx5.txt", O_RDONLY);
+
 	if (fd == -1)
 	{
 		perror("Error opening file");
 		return 1;
 	}
 	n = 0;
-	// while (n < 10)
-	// {
-	// 	line = get_next_line(fd);
-	// 	printf("%s", line);
-	// 	free(line);
-	// 	n++;
-	// }
-
+	printf("\nTEST 1\n");
 	while (n < 10)
 	{
 		line = get_next_line(fd);
 
 		printf("%s", line);
 		free(line);
-		// line = get_next_line(fd2);
-
-		// printf("%s", line);
-		// free(line);
 		n++;
 	}
+	n = 0;
+	printf("\nTEST 2\n");
+	while (n < 10)
+	{
+		line = get_next_line(fd2);
 
-	close(fd);
+		printf("%s", line);
+		free(line);
+		n++;
+	}
+	n = 0;
+	printf("\nTEST 3\n");
+	while (n < 10)
+	{
+		line = get_next_line(fd3);
+
+		printf("%s", line);
+		free(line);
+		n++;
+	}
+	n = 0;
+	printf("\nTEST 4\n");
+	while (n < 10)
+	{
+		line = get_next_line(fd4);
+
+		printf("%s", line);
+		free(line);
+		n++;
+	}
+	n = 0;
+	printf("\nTEST 5\n");
+	while (n < 10)
+	{
+		line = get_next_line(fd5);
+
+		printf("%s", line);
+		free(line);
+		n++;
+	}
+	n = 0;
+	printf("\nTEST 6\n");
+	while (n < 10)
+	{
+		line = get_next_line(fd6);
+
+		printf("%s", line);
+		free(line);
+		n++;
+	}
+	n = 0;
+	printf("\nTEST 7\n");
+	line = get_next_line(0);
+
+	printf("%s", line);
+	free(line);
+	line = get_next_line(0);
+
+	printf("%s", line);
+	free(line);
+
+
+	close(fd);\n
 	return 0;
 }
